6ec04c5dd3debdecbcff2d656c4b1246ccd19576
Priya <Priyamadiwalar0007@gmail.com>
2022-03-07 10:04:04 +0530

    Added: Number Of Cards Should be unique Deck

--- src/com/blz/deckofcards/DeckOfCards.java
+++ src/com/blz/deckofcards/DeckOfCards.java
@@ -1,3 +1,14 @@
+/*
+ * Write a Program to initialize deck of cards having suit ("Clubs", "Diamonds", "Hearts", "Spades") & Rank ("2", "3", "4", "5", "6", "7", "8", "9", "10","Jack", "Queen", "King", "Ace"). Shuffle the cards using Random method and then distribute 9 Cards to 4 Players and Print the Cards thereceived by the 4 Players.
+ *
+ * 1. Ability to work with GIT
+ * 2. Expected to have OOPs
+ * 3. Expected to have Data Structures
+ *
+ *UC1 : Setup the initial game with deck of cards and make sure we have unique cards, 
+ *The number of cards are 52. Should be able to make unique deck. Maintain the ranks to further compare
+ */
+
 package com.blz.deckofcards;
 
 import java.util.ArrayList;
@@ -6,6 +17,9 @@ public class DeckOfCards {
 
 	public ArrayList<String> cardsDeck = new ArrayList<String>();
 
+	/*
+	 * @purpose : initializing the 52 DeckOfCards to suits and ranks
+	 */
 	public void deckofCards() {
 		String[] suits = { "Clubs", "Diamonds", "Hearts", "Spades" };
 		String[] ranks = { "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "King", "Queen", "Ace" };
@@ -23,7 +37,7 @@ public class DeckOfCards {
 	 * initializing the deck of card
 	 */
 	public static void toDisplay(ArrayList<String> cardsDeck) {
-		// System.out.println("\nCards in Deck:");
+		System.out.println("\nCards in Deck:");
 		for (String element : cardsDeck) {
 			System.out.println(element);
 		}
