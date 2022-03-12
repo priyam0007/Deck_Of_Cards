2def79fede43aad4d4600b450119e5f5b6c1fa25
Priya <Priyamadiwalar0007@gmail.com>
2022-03-12 20:47:17 +0530

    Added: Each time we distribute the cards with the Player

--- src/com/blz/deckofcards/DeckOfCards.java
+++ src/com/blz/deckofcards/DeckOfCards.java
@@ -8,9 +8,9 @@
  * 2. Expected to have OOPs
  * 3. Expected to have Data Structures
  *
- * UC3 : Add the method to sequence the Players order and how they should receive 
- *       the cards and further order for play. 
-*/
+ * UC4 : Each time we distribute the cards with the Player, Condition is that we shuffle the
+ *  cards. Make a function to shuffle the cards.       
+ */
 
 package com.blz.deckofcards;
 
@@ -70,6 +70,25 @@ public class DeckOfCards {
 	 */
 	public void sequenceOfPlay(int player) {
 		System.out.println("\nSequence of cards are below : ");
+		toshuffle(cardsDeck, player);
+		System.out.println("Shuffling the cards before Distribution");
+	}
+
+	/*
+	 * @purpose: Each time we distribute the cards with the Player
+	 */
+
+	public static ArrayList<String> toshuffle(ArrayList<String> cardsDeck, int player) {
+		System.out.println("Shuffling the cards before Distribution");
+		ArrayList<String> temp = new ArrayList<String>();
+		while (!cardsDeck.isEmpty()) {
+			int loc = (int) (Math.random() * cardsDeck.size());
+			temp.add(cardsDeck.get(loc));
+			cardsDeck.remove(loc);
+		}
+		cardsDeck = temp;
+		toDisplay(cardsDeck);
+		return cardsDeck;
 	}
 
 	public static void main(String[] args) {
